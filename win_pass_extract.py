ba311d4c036183a88556372fd8e4de44815af9ba
vinaykagithapu <33367418+vinaykagithapu@users.noreply.github.com>
2021-06-04 15:37:29 +0530

    Update win_pass_extract.py

--- win_pass_extract.py
+++ win_pass_extract.py
@@ -1,18 +1,38 @@
 import subprocess
 from datetime import datetime
+from time import sleep
 
 #Banner
-print("-"*70)
-print("\t\tWelcome to V Wifi Password Extractor for Windows\n")
-print("\tTime started at\t: "+str(datetime.now()))
-print("-"*70,"\n\n")
+print("\n"+"#"*65)
+print("#\tWelcome to V WiFi Password Extractor for Windows\t#\n#\t\t\t\t\t\t\t\t#")
+print("#\tVisit: https://hackwithv.blogspot.com/\t\t\t#\n#\t\t\t\t\t\t\t\t#")
+print("#\tTime started at\t: "+str(datetime.now())+"\t\t#")
+print("#"*65+"\n\n")
+
+#Disclaimer
+print("Disclaimer: Developer assume no liability and are not responsible\n\t    for any misuse or damage caused by this tool.\n\n")
+
+ex=input("[+] Press Enter key to Start...")
+
+#Agree
+ag = 'null'
+ag = input("\n[!] Use this tool for educational purpose only\n[?] Are you agree? [ Y / N ] : ")
+if ag == 'y' or ag =='Y':
+        pass
+else:
+        print("\n[-] Please agree the tool, try again!")
+        ex=input("\n[!] Press Enter key to exit... ")
+        print("\n[+] Thanks for using V WiFi Password Extractor Tool\n")
+        sleep(2)
+        exit()
 
 #netsh process
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n') 
-profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i] 
+profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
 
-print("{:<30} | {:<}".format("Network Names", "Passwords"))
-print("{:<30}|{:<}".format("-"*31, "-"*25))
+print("\n+{:<20}+{:<}+".format("-"*30, "-"*32))
+print("{:<24} | {:<} \t\t|".format("|\tNetwork Name", "\tPassword"))
+print("+{:<20}+{:<}+".format("-"*30, "-"*32))
 
 for i in profiles:
 	try: 
@@ -20,10 +40,15 @@ for i in profiles:
 		results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
 		
 		try:
-			print ("{:<30} | {:<}".format(i, results[0]))
+			print ("{:<24} | {:<} \t\t|".format("|\t"+i, "\t"+results[0]))
 		except IndexError:
-			print ("{:<30}| {:<}".format(i, ""))
+			print ("{:<24} | {:<} \t\t|".format("|\t"+i, "\t\t"))
 	except subprocess.CalledProcessError:
-		print ("{:<30}| {:<}".format(i, "ENCODING ERROR"))
+		print ("{:<24} | {:<} \t\t|".format("|\t"+i, "\tENCODING ERROR"))
+print("+{:<20}+{:<}+".format("-"*30, "-"*32))
 #End of netsh
-print("\n\nThanking you, Bye !\n")
+
+ex=input("\n\n[!] Press Enter key to exit... ")
+print("\n[+] Thanks for using V WiFi Password Extractor Tool\n")
+sleep(3)
+exit()
